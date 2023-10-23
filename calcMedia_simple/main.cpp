d65df05aba2d68dc6396575ef16a24b44e82f2a6
Kato <kato8088@users.noreply.github.com>
2023-10-23 20:21:28 -0300

    VERSAO GIGATONICA v1.1

--- calcMedia_simple/main.cpp
+++ calcMedia_simple/main.cpp
@@ -6,22 +6,28 @@ using namespace std;
 int main()
 {
 
-    int media = 0;
-    int soma = 0;
-    int numeros;
-    int amount;
+    float media = 0;
+    float soma = 0;
+    float numeros;
+    float amount;
 
+    while (true) {
+        cout << "amount of items: ";
+        cin >> amount;
+        cout << "enter numbers: \n";
+        for (int i = 0; i < amount; i++)
+        {
+            cin >> numeros;
+            soma = soma + numeros;
+        }
+        media = soma / amount;
+        cout << "a mediana eh: " << media << endl;
 
-    cout << "amount of items: \n";
-    cin >> amount;
-    cout << "enter number: \n";
-    for (int i = 0; i < amount; i++)
-    {
-      //  cout << "Enter number: \n";
-        cin >> numeros;
-        soma = soma + numeros;
+        media = 0;
+        soma = 0;
+        amount = 0;
+        numeros = 0;
     }
-    media = soma / numeros;
-    cout << "a mediana eh: " << media << endl;
     return 0;
 }
+// versao meu mau na sua pao v1.1 - a atualizacao que refinou o codigo !!!!! :ooooooo
